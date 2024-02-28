335059d514ef67b56c77e5951bcdce6a44860da2
Federico Mederos <fedemederos@MacBook-Pro-de-Federico.local>
2024-02-27 22:00:27 -0300

    se le agrega interfaz al juego

--- script.js
+++ script.js
@@ -1,63 +1,89 @@
-// Genera un numero random para la opción de la PC
-function getComputerChoice(){
-    var computerChoice = Math.floor(Math.random()*3)+1;
-    if(computerChoice == 1){
-        return 'PIEDRA'
-    }else if(computerChoice == 2){
-        return 'PAPEL'
-    }else { return 'TIJERA'}
-}
 
-// Compara las opciones para definir el ganador
-function playRound(playerSelection, computerChoice){
+        var playerScore = 0
+        var computerScore = 0
+        
+        const selectPiedra = document.querySelector('#piedra');
+        const selectPapel = document.querySelector('#papel');
+        const selectTijera = document.querySelector('#tijera');
+        const resetGame = document.querySelector('#resetGame');
+        const textoInformativo = document.createElement('p');
+        const resultados = document.createElement('p');
+        const informacion = document.querySelector('.informacion');
+        
+        selectPiedra.addEventListener('click', () => {
+            playerSelection = 'PIEDRA';
+            computerChoice = getComputerChoice();
+            playRound(playerSelection, computerChoice);
+            finalResult(playerScore, computerScore);
+        })
+        
+        selectPapel.addEventListener('click', () => {
+            playerSelection = 'PAPEL';
+            computerChoice = getComputerChoice();
+            playRound(playerSelection, computerChoice);
+            finalResult(playerScore,computerScore);
+        })
+        
+        selectTijera.addEventListener('click', () =>{
+            playerSelection = 'TIJERA';
+            computerChoice = getComputerChoice();
+            playRound(playerSelection, computerChoice);
+            finalResult(playerScore,computerScore);
+        })
+        
+        
+        resetGame.addEventListener('click', () => {
+            playerScore = 0;
+            computerScore = 0;
+            console.log(computerScore, playerScore);
+        })
+        
+        
+        function finalResult(){
+            if(playerScore === 5 || computerScore === 5){
+                if (computerScore > playerScore){
+                    textoInformativo.textContent = `LA CPU ha ganado 5 rondas antes que tu 😆`
+                }else (playerScore > computerScore);
+                textoInformativo.textContent = `Felicitaciones has ganado 5 rondas antes que la CPU 😎`
+            }else;
+        }
     
-    if(playerSelection === computerChoice){
-        alert('Empate - '+ 'PC: '+computerChoice + ' TU: '+playerSelection)
-    } else if(playerSelection === 'PIEDRA' && computerChoice === 'PAPEL'){
-        alert('Perdiste - '+ 'PC: '+computerChoice + ' TU: '+playerSelection)
-        computerScore++; 
-    } else if(playerSelection === 'PAPEL' && computerChoice === 'TIJERA'){
-        alert('Perdiste - '+ 'PC: '+computerChoice + ' TU: '+playerSelection)
-        computerScore++;
-    } else if(playerSelection === 'TIJERA' && computerChoice === 'PIEDRA'){
-        alert('Perdiste - '+ 'PC: '+computerChoice + ' TU: '+playerSelection)
-        computerScore++;
-    } else if(playerSelection === 'PIEDRA' && computerChoice === 'TIJERA'){
-        alert('Ganaste - '+ 'PC: '+computerChoice + ' TU: '+playerSelection)
-        playerScore++;
-    } else if(playerSelection === 'PAPEL' && computerChoice === 'PIEDRA'){
-        alert('Ganaste - '+ 'PC: '+computerChoice + ' TU: '+playerSelection)
-        playerScore++;
-    } else {
-        alert('Ganaste - '+ 'PC: '+computerChoice + ' TU: '+playerSelection)
-        playerScore++;
-    }
-}
-// Solicita ingresar la opción de jugador y repite 5 veces el PlayRound 
-function playGame(){
-    for (let i = 0; i <= 4; i ++){
-        let playerSelection = prompt('Por favor ingrese piedra papel o tijera:').toUpperCase();
-        computerChoice = getComputerChoice()
-        playRound(playerSelection, computerChoice) 
-    }
-}
-// compara los resultados de cada jugador e informa al ganador
-function finalResult(){
-    if(playerScore > computerScore){
-        alert ('Fuiste el mejor de las 5 rondas')
-    } else if (computerScore > playerScore){
-        alert ('La computadora te ha ganado')
-    }else {alert ('Fue un justo empate')}
-}
-// Reset de los scores
-function resetResults(playerScore, computerScore) {
-    playerScore = 0
-    computerScore = 0
-}
+        
 
-let playerScore = 0
-let computerScore = 0
-
-playGame()
-finalResult()
-resetResults(playerScore, computerScore)
+        // Genera un numero random para la CPU
+        function getComputerChoice(){
+            var computerChoice = Math.floor(Math.random()*3)+1;
+            if(computerChoice == 1){
+                return 'PIEDRA'
+            }else if(computerChoice == 2){
+                return 'PAPEL'
+            }else { return 'TIJERA'}
+        }
+        
+        // Compara las opciones para definir el ganador
+        function playRound(playerSelection, computerChoice){
+            if(playerSelection === computerChoice){
+                textoInformativo.textContent = `La ronda fue un empate`
+            } else if(playerSelection === 'PIEDRA' && computerChoice === 'PAPEL'){
+                textoInformativo.textContent = `Perdiste, la CPU gano la ronda`
+                computerScore++; 
+            } else if(playerSelection === 'PAPEL' && computerChoice === 'TIJERA'){
+                textoInformativo.textContent = `Perdiste, la CPU gano la ronda`
+                computerScore++;
+            } else if(playerSelection === 'TIJERA' && computerChoice === 'PIEDRA'){
+                textoInformativo.textContent = `Perdiste, la CPU gano la ronda`
+                computerScore++;
+            } else if(playerSelection === 'PIEDRA' && computerChoice === 'TIJERA'){
+                textoInformativo.textContent = `Has ganado la ronda!`
+                playerScore++;
+            } else if(playerSelection === 'PAPEL' && computerChoice === 'PIEDRA'){
+                textoInformativo.textContent = `Has ganado la ronda!`
+                playerScore++;
+            } else {
+                textoInformativo.textContent = `Has ganado la ronda!`
+                playerScore++;
+            }
+        resultados.textContent = `CPU ${computerScore} // Player ${playerScore}`
+        informacion.appendChild(textoInformativo);
+        informacion.appendChild(resultados);
+        }
